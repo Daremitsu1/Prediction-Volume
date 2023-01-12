095f6eaa9379a8b7156563dfd61cf15e00fd4703
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2023-01-12 11:05:07 +0530

    Create recommender.R

---
+++ recommender.R
@@ -0,0 +1,35 @@
+# Import the necessary libraries
+library(dplyr)
+
+# Read in the data
+sales_data <- read.csv("sales_data.csv")
+
+# Use dplyr to get a summary of the data
+sales_summary <- sales_data %>% 
+  group_by(Product_Category, Sub_Category, Product) %>% 
+  summarize(mean_quantity = mean(Order_Quantity),
+            mean_unit_price = mean(Unit_Price),
+            mean_unit_cost = mean(Unit_Cost),
+            mean_profit = mean(Profit))
+
+# Print the summary
+print(sales_summary)
+
+# Use dplyr to look at average customer age by gender
+avg_age_by_gender <- sales_data %>% 
+  group_by(Customer_Gender) %>% 
+  summarize(mean_age = mean(Customer_Age))
+
+# Print the result
+print(avg_age_by_gender)
+
+# Use dplyr to find the top 5 states with the most revenue
+top_states_by_revenue <- sales_data %>% 
+  group_by(State) %>% 
+  summarize(total_revenue = sum(Revenue)) %>% 
+  arrange(desc(total_revenue)) %>% 
+  head(5)
+
+# Print the result
+print(top_states_by_revenue)
+
